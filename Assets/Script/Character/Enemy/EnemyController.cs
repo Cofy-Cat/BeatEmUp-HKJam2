30dc94322f35863ed8226b37eb6487a9a9f4d5e7
lokyinjason <lokyinjason921@gmail.com>
2024-10-12 00:02:58 +0800

    Enemy move towards player

--- Assets/Script/Character/Enemy/EnemyController.cs
+++ Assets/Script/Character/Enemy/EnemyController.cs
@@ -1,7 +1,34 @@
+using System;
+using cfEngine.Logging;
+using UnityEngine;
+using UnityEngine.Rendering;
+
 public class EnemyController: Controller
 {
+    [SerializeField] private GameObject player;
+    private Vector3 direction;
+    private Vector2 input;
+
+    private void Awake(){
+        input = Vector2.zero;
+        direction = Vector3.zero;
+    }
     private void Start()
     {
         _command.ExecuteCommand(new IdleCommand());
+    }
+
+    private void FixedUpdate() {
+        direction = player.transform.position - transform.position;
+        direction.Normalize();
+        Debug.Log($"Input: {input}");
+        Debug.Log($"Direction xy: {direction.x}, {direction.y}");
+
+        if (input != new Vector2(direction.x, direction.y)) {
+            _command.ExecuteCommand(new IdleCommand());
+            Debug.Log("Move");
+            input = new Vector2(direction.x, direction.y);
+            _command.ExecuteCommand(new MoveCommand(input));
+        }
     }
 }
