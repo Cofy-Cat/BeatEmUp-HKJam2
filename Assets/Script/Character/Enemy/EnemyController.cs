3796eae94ad0d65d917dfc4d3aac011a2269a088
Jason Tung <lokyinjason921@gmail.com>
2024-10-13 00:26:39 +0800

    Enemy chase player in range
    - Idle -> chase when near -> idle when far

--- Assets/Script/Character/Enemy/EnemyController.cs
+++ Assets/Script/Character/Enemy/EnemyController.cs
@@ -8,21 +8,28 @@ public class EnemyController : Controller
     [SerializeField] private GameObject player;
     private Vector3 direction;
     private Vector2 input;
-    private Boolean isTriggered = false;
-
+    private bool isTriggered = false;
     protected override void Awake()
     {
         base.Awake();
         input = Vector2.zero;
         direction = Vector3.zero;
+    }
+    private void OnEnable()
+    {
         _shadow.triggerEnter += OnShadowTriggerEnter;
         _shadow.triggerExit += OnShadowTriggerExit;
+    }
+    private void OnDisable()
+    {
+        _shadow.triggerEnter -= OnShadowTriggerEnter;
+        _shadow.triggerExit -= OnShadowTriggerExit;
     }
     private void OnShadowTriggerEnter(Collider2D collider)
     {
         Debug.Log($"OnShadowTriggerEnter: " + collider.name);
         isTriggered = true;
-        Hurt();
+        _command.ExecuteCommand(new IdleCommand());
     }
 
     private void OnShadowTriggerExit(Collider2D collider)
@@ -38,21 +45,35 @@ public class EnemyController : Controller
 
     private void FixedUpdate()
     {
-        direction = player.transform.position - transform.position;
-        direction.Normalize();
-
-        if (input != new Vector2(direction.x, direction.y) && !isTriggered)
-        {
-            Debug.Log("Move");
-            Debug.Log($"Input: {input}");
-            Debug.Log($"Direction xy: {direction.x}, {direction.y}");
-            input = new Vector2(direction.x, direction.y);
-            _command.ExecuteCommand(new MoveCommand(input));
-        }
+        if (ifPlayerIsNear()) ChasePlayer();
+        else _command.ExecuteCommand(new IdleCommand());
     }
 
     public void Hurt()
     {
         _command.ExecuteCommand(new HurtCommand());
+    }
+
+    private bool ifPlayerIsNear()
+    {
+        if (Vector3.Distance(player.transform.position, transform.position) < 5f)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private void ChasePlayer()
+    {
+        direction = player.transform.position - transform.position;
+        direction.Normalize();
+        if (input != new Vector2(direction.x, direction.y) && !isTriggered)
+        {
+            input = new Vector2(direction.x, direction.y);
+            _command.ExecuteCommand(new MoveCommand(input));
+        }
     }
 }
