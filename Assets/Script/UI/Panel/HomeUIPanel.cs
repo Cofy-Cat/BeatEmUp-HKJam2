e725c33bb2c109b6177934af269869109325d369
felixwongong <74048524+felixwongong@users.noreply.github.com>
2024-10-17 09:55:09 +0800

    add basic home scene

---
+++ Assets/Script/UI/Panel/HomeUIPanel.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class HomeUIPanel : MonoBehaviour
+{
+    [SerializeField] private Button startButton;
+        
+    [SerializeField] private string nextScene;
+
+    private void OnEnable()
+    {
+        startButton.onClick.AddListener(OnStartClicked);
+    }
+
+    private void OnDisable()
+    {
+        startButton.onClick.RemoveListener(OnStartClicked);
+    }
+
+    private void OnStartClicked()
+    {
+        
+    }
+
+    private IEnumerator StartSequenceRoutine()
+    {
+        yield return new WaitForSeconds(2f);
+
+        SceneManager.LoadScene(nextScene);
+    }
+}
