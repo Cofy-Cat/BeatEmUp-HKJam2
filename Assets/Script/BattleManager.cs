2b1bf3f9e0c3b8348d7075656c737008d73bad46
felixwongong <74048524+felixwongong@users.noreply.github.com>
2024-10-19 14:55:47 +0800

    add next level collider

--- Assets/Script/BattleManager.cs
+++ Assets/Script/BattleManager.cs
@@ -1,11 +1,36 @@
+using System;
+using System.Collections.Generic;
+using cfEngine.Logging;
 using UnityEngine;
 
 public class BattleManager : MonoBehaviour
 {
     [SerializeField] private AudioClip bgm;
-    
+    [SerializeField] private List<Controller> enemies;
+
     void Start()
     {
         AudioManager.Instance.PlayBgm(bgm, 1f);
+    }
+
+    private void OnEnable()
+    {
+        foreach (var enemy in enemies)
+        {
+            enemy.onDead += EnemyOnDead;
+        }
+    }
+
+    private void OnDisable()
+    {
+        foreach (var enemy in enemies)
+        {
+            enemy.onDead -= EnemyOnDead;
+        }
+    }
+    
+    private void EnemyOnDead()
+    {
+        Log.LogInfo("Enemy all dead");
     }
 }
